d50bd715858fde97eaccbd3ae498941b3f12a342
shota-hope <ddeeppaa.gogo@gmail.com>
2023-09-17 12:50:47 +0900

    finish CH4

--- spec/dollar_spec.rb
+++ spec/dollar_spec.rb
@@ -4,14 +4,12 @@ require 'dollar'
 RSpec.describe Dollar do
   it 'multiplies the amount by a factor' do
     five = Dollar.new(5)
-    product = five.times(2)
-    expect(product.amount).to eq(10)
+    expect(five.times(2).amount).to eq(Dollar.new(10).amount)
     product = five.times(3)
-    expect(product.amount).to eq(15)
+    expect(five.times(3).amount).to eq(Dollar.new(15).amount)
   end
 
   it '#epuals' do
-
     expect(Dollar.new(5).equals(Dollar.new(5))).to be_truthy
     expect(Dollar.new(5).equals(Dollar.new(6))).to be_falsey
   end
