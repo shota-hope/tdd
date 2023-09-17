dc13c3b5dd082a6d71583c366aebec6c7f9e64a7
shota-hope <ddeeppaa.gogo@gmail.com>
2023-09-17 14:00:30 +0900

    finish Ch8

--- lib/money.rb
+++ lib/money.rb
@@ -8,4 +8,12 @@ class Money
   def equals(money)
     @amount == money.amount && self.class == money.class
   end
+
+  def self.dollar(amount)
+    Dollar.new(amount)
+  end
+
+  def self.franc(amount)
+    Franc.new(amount)
+  end
 end
--- spec/dollar_spec.rb
+++ spec/dollar_spec.rb
@@ -4,24 +4,24 @@ require 'franc'
 
 RSpec.describe Dollar do
   it 'multiplies the amount by a factor' do
-    five = Dollar.new(5)
-    expect(five.times(2).amount).to eq(Dollar.new(10).amount)
+    five = Money.dollar(5)
+    expect(five.times(2).amount).to eq(Money.dollar(10).amount)
     product = five.times(3)
-    expect(five.times(3).amount).to eq(Dollar.new(15).amount)
+    expect(five.times(3).amount).to eq(Money.dollar(15).amount)
   end
 
   it '#epuals' do
-    expect(Dollar.new(5).equals(Dollar.new(5))).to be_truthy
-    expect(Dollar.new(5).equals(Dollar.new(6))).to be_falsey
-    expect(Franc.new(5).equals(Franc.new(5))).to be_truthy
-    expect(Franc.new(5).equals(Franc.new(6))).to be_falsey
-    expect(Franc.new(5).equals(Dollar.new(5))).to be_falsey
+    expect(Money.dollar(5).equals(Money.dollar(5))).to be_truthy
+    expect(Money.dollar(5).equals(Money.dollar(6))).to be_falsey
+    expect(Money.franc(5).equals(Money.franc(5))).to be_truthy
+    expect(Money.franc(5).equals(Money.franc(6))).to be_falsey
+    expect(Money.franc(5).equals(Money.dollar(5))).to be_falsey
   end
 
   it 'test franc mutipication' do
     five = Franc.new(5)
-    expect(five.times(2).amount).to eq(Franc.new(10).amount)
+    expect(five.times(2).amount).to eq(Money.franc(10).amount)
     product = five.times(3)
-    expect(five.times(3).amount).to eq(Franc.new(15).amount)
+    expect(five.times(3).amount).to eq(Money.franc(15).amount)
   end
 end
