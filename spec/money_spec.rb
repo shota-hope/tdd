64f10ae0b0ad46c0fe4588df631b91fe38471305
shota-hope <ddeeppaa.gogo@gmail.com>
2023-09-23 16:52:00 +0900

    finish ch11

--- spec/money_spec.rb
+++ spec/money_spec.rb
@@ -1,21 +1,11 @@
 require 'rspec'
-require 'dollar'
 require 'franc'
 require 'pry'
 
 RSpec.describe Money do
-  it 'multiplies the amount by a factor' do
-    five = Money.dollar(5)
-    expect(five.times(2).amount).to eq(Money.dollar(10).amount)
-    product = five.times(3)
-    expect(five.times(3).amount).to eq(Money.dollar(15).amount)
-  end
-
   it '#epuals' do
     expect(Money.dollar(5).equals(Money.dollar(5))).to be_truthy
     expect(Money.dollar(5).equals(Money.dollar(6))).to be_falsey
-    expect(Money.franc(5).equals(Money.franc(5))).to be_truthy
-    expect(Money.franc(5).equals(Money.franc(6))).to be_falsey
     expect(Money.franc(5).equals(Money.dollar(5))).to be_falsey
   end
 
@@ -29,9 +19,5 @@ RSpec.describe Money do
   it 'currency' do
     expect(Money.dollar(1).currency).to eq("USD")
     expect(Money.franc(1).currency).to eq("CHF")
-  end
-
-  it 'differenc class equality' do
-    expect(Money.new(10, "CHF").equals(Franc.new(10, "CHF"))).to be_truthy
   end
 end
