4d62f6823bad88c8a7e86f713be1183d161bb7f1
shota-hope <ddeeppaa.gogo@gmail.com>
2023-10-09 17:55:40 +0900

    finish ch15

--- spec/money_spec.rb
+++ spec/money_spec.rb
@@ -61,4 +61,14 @@ RSpec.describe Money do
   it 'identity rate' do
     expect(Bank.new.rate('USD', 'USD')).to eq(1)
   end
+
+  it 'tests mixed addition' do
+    five_bucks = Money.dollar(5)
+    ten_francs = Money.franc(10)
+    bank = Bank.new
+    bank.add_rate("CHF", "USD", 2)
+
+    result = bank.reduce(five_bucks.plus(ten_francs), "USD")
+    expect(result.amount).to eq(Money.dollar(10).amount)
+  end
 end
